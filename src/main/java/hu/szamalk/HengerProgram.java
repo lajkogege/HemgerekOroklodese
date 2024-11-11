9ed56fccc25c147381a2a2f360f9ea0a231b3986
lajkogege <lajkogege@gmail.com>
2024-11-11 09:32:09 +0100

    feldatok main-ben

---
+++ src/main/java/hu/szamalk/HengerProgram.java
@@ -0,0 +1,30 @@
+package hu.szamalk;
+
+import hu.szamalk.Model.MertaniHenger;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HengerProgram {
+
+    public HengerProgram() {
+        hengerek = new ArrayList<>();
+    }
+
+    public double atlagTerfogat(){
+        return 0.0;
+    }
+
+    public double csovekSulya(){
+        return 0.0;
+    }
+
+    private List<MertaniHenger> hengerek;
+
+    public void run(){
+
+    }
+
+
+}
